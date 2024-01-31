4e4d9e69745e9cbc7cb8b2000df4bc240236cef8
Nicholas FitzRoy-Dale <nicholas@fitzroydale.org>
2024-01-31 23:36:42 +0000

    Move execution loop out of main

--- Makefile
+++ Makefile
@@ -7,7 +7,8 @@
 # 8086tiny_slowcpu improves graphics performance on slow platforms (e.g. Raspberry Pi)
 # no_graphics compiles without SDL graphics/sound
 
-OPTS_ALL=-O3 -fsigned-char -std=c99
+# OPTS_ALL=-O3 -fsigned-char -std=c99
+OPTS_ALL=-g -fsigned-char -std=c99
 OPTS_SDL=`sdl-config --cflags --libs`
 OPTS_NOGFX=-DNO_GRAPHICS
 OPTS_SLOWCPU=-DGRAPHICS_UPDATE_DELAY=25000
@@ -18,16 +19,16 @@ bios.bin: bios.asm
 tinyxms.sys: tinyxms.asm
 	nasm -f bin -I ../lmacros/ -I lmacros/ -l tinyxms.lst -o tinyxms.sys tinyxms.asm
 
-8086tiny: 8086tiny.c bios.bin tinyxms.sys
-	${CC} 8086tiny.c ${OPTS_SDL} ${OPTS_ALL} -o 8086tiny
+8086tiny: 8086tiny.c bios.bin tinyxms.sys x86.c x86.h
+	${CC} 8086tiny.c x86.c ${OPTS_SDL} ${OPTS_ALL} -o 8086tiny
 	strip 8086tiny
 
-8086tiny_slowcpu: 8086tiny.c bios.bin tinyxms.sys
-	${CC} 8086tiny.c ${OPTS_SDL} ${OPTS_ALL} ${OPTS_SLOWCPU} -o 8086tiny
+8086tiny_slowcpu: 8086tiny.c bios.bin tinyxms.sys x86.c x86.h
+	${CC} 8086tiny.c x86.c ${OPTS_SDL} ${OPTS_ALL} ${OPTS_SLOWCPU} -o 8086tiny
 	strip 8086tiny
 
 no_graphics: 8086tiny.c bios.bin tinyxms.sys
-	${CC} 8086tiny.c ${OPTS_NOGFX} ${OPTS_ALL} -o 8086tiny
+	${CC} 8086tiny.c x86.c ${OPTS_NOGFX} ${OPTS_ALL} -std=gnu99 -o 8086tiny
 	strip 8086tiny
 
 clean:
---
+++ x86.h
@@ -0,0 +1,66 @@
+#include <time.h>
+#include <sys/timeb.h>
+
+// Emulator system constants
+#define IO_PORT_COUNT 0x10000
+#define RAM_SIZE 0x10FFF0
+#define REGS_BASE 0xF0000
+#define VIDEO_RAM_SIZE 0x10000
+
+// 16-bit register decodes
+#define REG_AX 0
+#define REG_CX 1
+#define REG_DX 2
+#define REG_BX 3
+#define REG_SP 4
+#define REG_BP 5
+#define REG_SI 6
+#define REG_DI 7
+
+#define REG_ES 8
+#define REG_CS 9
+#define REG_SS 10
+#define REG_DS 11
+
+#define REG_ZERO 12
+#define REG_SCRATCH 13
+
+// 8-bit register decodes
+#define REG_AL 0
+#define REG_AH 1
+#define REG_CL 2
+#define REG_CH 3
+#define REG_DL 4
+#define REG_DH 5
+#define REG_BL 6
+#define REG_BH 7
+
+// FLAGS register decodes
+#define FLAG_CF 40
+#define FLAG_PF 41
+#define FLAG_AF 42
+#define FLAG_ZF 43
+#define FLAG_SF 44
+#define FLAG_TF 45
+#define FLAG_IF 46
+#define FLAG_DF 47
+#define FLAG_OF 48
+
+struct x86_state {
+	void (*redraw_display)(struct x86_state *);
+	void (*keyboard_driver)(struct x86_state *);
+	int op_result, disk_bios, disk_fdd, disk_hdd;
+	time_t clock_buf;
+	struct timeb ms_clock;
+	unsigned int op_source, op_dest, rm_addr, op_to_addr, op_from_addr, i_data0, i_data1, i_data2, scratch_int, scratch_uint, scratch2_uint, keyboard_timer_inst_counter, graphics_inst_counter, set_flags_type, GRAPHICS_X, GRAPHICS_Y, pixel_colors[16];
+	unsigned short *regs16, reg_ip, seg_override, wave_counter, reg_ip_before_rep_trace;
+	unsigned char *opcode_stream, *regs8, i_rm, i_w, i_reg, i_mod, i_mod_size, i_d, i_reg4bit, raw_opcode_id, xlat_opcode_id, extra, rep_mode, seg_override_en, rep_override_en, trap_flag, int8_asap, scratch_uchar, io_hi_lo, *vid_mem_base, spkr_en, hlt_this_time, setting_ss, prior_setting_ss, reset_ip_after_rep_trace, shift_count;
+	unsigned char io_ports[IO_PORT_COUNT + 16];
+	unsigned char mem[RAM_SIZE + 16];
+};
+
+struct x86_state *x86_init(int boot_from_hdd, char *bios_filename, char *fdd_filename, char *hdd_filename, void(*redraw_display)(struct x86_state *), void(*keyboard_driver)(struct x86_state *));
+void x86_free(struct x86_state *);
+void x86_step(struct x86_state *);
+//char set_CF(struct x86_state *s, int new_CF);
+//char pc_interrupt(struct x86_state *s, unsigned char interrupt_num);
